71d1a274ed319e6dc20b83b3f479a08620c0aa18
JossElito179 <118587500+JossElito179@users.noreply.github.com>
2026-01-17 12:44:40 +0300

    Update login.tsx

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,11 +1,10 @@
-import Ballpit from "../bits-components/BallPit";
 import LoginComponent from "../components/login_component";
 
 export default function Login() {
 
     return (
         <>
-            <div className="bg-gradient-to-b from-gray-800 via-gray-900 to-black min-h-screen" >
+            <div className="bg-linear-to-b from-gray-800 via-gray-900 to-black min-h-screen" >
                 <LoginComponent />
             </div>
         </>
