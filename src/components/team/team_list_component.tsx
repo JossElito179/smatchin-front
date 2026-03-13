a7b9668df2f9e667df6903fee0aa668b9840e04d
JossElito179 <118587500+JossElito179@users.noreply.github.com>
2026-03-13 13:10:42 +0300

    updating access

--- src/components/team/team_list_component.tsx
+++ src/components/team/team_list_component.tsx
@@ -437,16 +437,16 @@ export default function TeamListComponent() {
                                                             onRemove={handleRemove}
                                                             loading={loadingId === row.id}
                                                         />
-                                                    // ) : user?.role == false && (user?.isStaff == true || user?.canModify == true) && Array.isArray(canHandledResponse) &&
-                                                    //     canHandledResponse.some((p: any) => p.id === row.id) ? (
-                                                    //     <TeamActions
-                                                    //         team={row}
-                                                    //         onViewDetails={handleViewDetails}
-                                                    //         onViewPlayers={handleViewPlayers}
-                                                    //         onUpdate={handleUpdate}
-                                                    //         onRemove={handleRemove}
-                                                    //         loading={loadingId === row.id}
-                                                    //     />
+                                                    ) : user?.role == false && (user?.isStaff == true || user?.canModify == true) && Array.isArray(canHandledResponse) &&
+                                                        canHandledResponse.some((p: any) => p.id === row.id) ? (
+                                                        <TeamActions
+                                                            team={row}
+                                                            onViewDetails={handleViewDetails}
+                                                            onViewPlayers={handleViewPlayers}
+                                                            onUpdate={handleUpdate}
+                                                            onRemove={handleRemove}
+                                                            loading={loadingId === row.id}
+                                                        />
                                                     ) : (
                                                         <TeamActionsAvaibs
                                                             team={row}
